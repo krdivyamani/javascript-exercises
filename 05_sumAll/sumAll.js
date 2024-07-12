ab59de806bed2d0d019c8ebb7e6110ce4fa0d4fb
krdivyamani <dyivyamani@gmail.com>
2024-07-12 09:56:27 +0530

    Added !Number.isInteget(num) to check for int, and returned error where required.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    let ans = 0;
+    const start = Math.min(num1, num2);
+    const end = Math.max(num1, num2)
+    for(i = start; i <= end; i ++){
+        ans += i;
+    }
+    return ans
 };
 
 // Do not edit below this line
