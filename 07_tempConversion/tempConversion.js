0b7d03753703114b4665001b293a2502b06445e1
krdivyamani <dyivyamani@gmail.com>
2024-07-12 10:43:49 +0530

    Added formula to convert temp between c <-> f

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let ans = (5/9) * (fahrenheit - 32);
+  return Math.round(ans * 10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let ans = celsius * (9/5) + 32;
+  return Math.round(ans * 10)/10
 };
 
 // Do not edit below this line
