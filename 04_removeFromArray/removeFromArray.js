146fc6b1d0928203e1ebbd518e1028248b19cf65
krdivyamani <dyivyamani@gmail.com>
2024-07-12 09:45:01 +0530

    Added .filter(callback) to scan elements and create new array a/c condition.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // Function Declaration
+    // return array.filter(function(val){
+    //     return !args.includes(val)
+    // });
+    
+    // Arrow Function
+    return array.filter(val => !args.includes(val))
 };
 
 // Do not edit below this line
